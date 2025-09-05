6b0ed31feb45481d22925fb2e642d2051b2f4e1d
Emerson Paduan <emerson@paduan.dev.br>
2025-09-04 22:53:54 -0300

    aula 02

---
+++ aula02/exemplo01.py
@@ -0,0 +1,21 @@
+# Esta linha é um comentário
+
+# Primeiro programa em Python
+# 29/08/2025
+
+#exibir dados na tela
+print("Boa noite!")
+print('Boa noite!')
+
+print('12 + 3')
+print(12 + 3)
+
+print(1, "oi", 3)
+print(1, "oi", 3, sep=',') # sep indica o que será usado para separar as informações na tela
+print(1, "oi", 3, sep=', ')
+
+print('a', 'b', 'c', end='-') # end indica o que será usado no final do print (nesse caso '-')
+print('a', 'b', 'c', end='-\n') # end indica '-' e depois pula para a próxima linha
+print('a', 'b', 'c', end='-')
+
+print('Ola\nBoa noite\nComo vai?') # por padrão o end pula para a próxima linha
---
+++ aula02/exemplo02.py
@@ -0,0 +1,12 @@
+# operadores aritméticos
+# + - * /
+
+print( 5 * 2 )
+print( 5 + 2 * 3)
+print( (5 + 2) * 3)
+
+print( 5 / 2 ) # divisão com decimal
+print( 5 // 2 ) # divisão calculando apenas a parte inteira da resposta
+
+print( 5 / 3 )
+print( 5 // 3 )
