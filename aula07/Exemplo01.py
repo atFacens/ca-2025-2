d07dc790145371a1fbede214db7246a89f9a0fa2
Emerson Paduan <emerson@paduan.dev.br>
2025-10-03 23:21:25 -0300

    aula 07

---
+++ aula07/Exemplo01.py
@@ -0,0 +1,8 @@
+cont = 1 # inicialização
+
+while(cont <= 10): # teste de condição, termina o laço (loop)
+    print(cont)
+    cont = cont + 2 # passo: muda ou altera a condição
+
+print('o valor final do cont é', cont)
+print('terminou')
---
+++ aula07/Exemplo02.py
@@ -0,0 +1,8 @@
+cont = 1 
+
+limite = int(input('Informe o limite da contagem: '))
+
+while(cont <= limite): 
+    print(cont)
+    cont = cont + 1
+
