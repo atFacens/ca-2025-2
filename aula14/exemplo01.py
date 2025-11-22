3c8a96f5f9d947033b19a4c05ee747a61187af3c
Emerson Paduan <emerson@paduan.dev.br>
2025-11-21 23:17:39 -0300

    aula 14

---
+++ aula14/exemplo01.py
@@ -0,0 +1,19 @@
+# lista = [1,2,3,4]
+
+# lista[1] = 5
+
+# print(lista)
+
+# matriz = [[1,2,3],[4,5,6],[7,8,9]]
+
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+print(matriz)
+
+matriz[1][2] = 0
+
+print(matriz)
---
+++ aula14/exemplo02.py
@@ -0,0 +1,15 @@
+matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# matriz[0][0] = 1
+# matriz[0][1] = 2
+# matriz[0][2] = 3
+# matriz[1][0] = 4 ...
+
+for linha in range(3):
+    for coluna in range(3):
+        print('matiz[',linha,'][', coluna,'] = ',matriz[linha][coluna])
+
